2e3dd6475a9f655cd7a5dd9621be2ef94e9d95d6
Francisco Quintero Linux Mint <frajaquico@gmail.com>
2014-12-31 17:23:37 -0500

    cambiar nombre de aplicacion. cambiar icono de aplicacion

--- www/js/languages.js
+++ www/js/languages.js
@@ -1,7 +1,7 @@
 var english = {
 	HOME: {
-		HEADLINE: 'Yify Movies',
-		INTROTEXT: 'Find movies with the best quality in Yify',
+		HEADLINE: 'Movie Searcher',
+		INTROTEXT: 'Find your favorite movies with great quality (720p and 1080p)',
 		PLACEHOLDER: 'Type in the name of the movie',
 		BUTTON_TEXT: 'Find movie!'
 	},
@@ -24,8 +24,8 @@ var english = {
 
 var spanish = {
 	HOME: {
-		HEADLINE: 'Yify Movies',
-		INTROTEXT: 'Encuentra las mejores películas en Yify',
+		HEADLINE: 'Movie Searcher',
+		INTROTEXT: 'Encuentra las mejores películas con muy buena calidad',
 		PLACEHOLDER: 'Escribe el nombre de la película',
 		BUTTON_TEXT: 'Buscar película!'
 	},
